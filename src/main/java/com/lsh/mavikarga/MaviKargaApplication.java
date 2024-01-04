25f0a1cadace5e629a91e9bafd2d3e0e2572c065
LSH <dltpgustpgus@gmail.com>
2024-01-04 18:56:42 +0900

    feat(initial)

---
+++ src/main/java/com/lsh/mavikarga/MaviKargaApplication.java
@@ -0,0 +1,13 @@
+package com.lsh.mavikarga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaviKargaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaviKargaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lsh/mavikarga/MaviKargaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsh.mavikarga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaviKargaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
