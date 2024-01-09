cf37379f960cb2c6451e3d6ef477eeadbcde490b
LSH <dltpgustpgus@gmail.com>
2024-01-09 14:59:29 +0900

    feat(navbar): 현재 페이지에 따라 th:classappend 로 active, fw-bold 추가해서 현재 페이지 버튼 잘보이도록 처리

--- src/main/java/com/lsh/mavikarga/controller/PageController.java
+++ src/main/java/com/lsh/mavikarga/controller/PageController.java
@@ -1,18 +1,21 @@
 package com.lsh.mavikarga.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("menu", "home");
         return "index";
     }
 
     @GetMapping("/clothing")
-    public String clothing() {
+    public String clothing(Model model) {
+        model.addAttribute("menu", "clothing");
         return "clothing";
     }
 }
