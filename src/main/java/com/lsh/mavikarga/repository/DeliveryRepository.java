acfbd802ba45e462853057e5cd757d2590cf93f8
LSH <dltpgustpgus@gmail.com>
2024-02-27 13:52:06 +0900

    webhook test...

--- src/main/java/com/lsh/mavikarga/repository/DeliveryRepository.java
+++ src/main/java/com/lsh/mavikarga/repository/DeliveryRepository.java
@@ -4,7 +4,4 @@ import com.lsh.mavikarga.domain.Delivery;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
-
-    Delivery findByMerchantUid(String merchantUid);
-
 }
