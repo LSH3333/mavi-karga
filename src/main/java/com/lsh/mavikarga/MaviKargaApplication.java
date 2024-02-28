bd24fca3a74867e9910c1512c8b5e19b23ef53a8
LSH <dltpgustpgus@gmail.com>
2024-02-28 12:26:49 +0900

    feat: aws ec2 에서 한국 타임존으로 변경 위해 postConstruct 추가함

--- src/main/java/com/lsh/mavikarga/MaviKargaApplication.java
+++ src/main/java/com/lsh/mavikarga/MaviKargaApplication.java
@@ -1,12 +1,22 @@
 package com.lsh.mavikarga;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @EnableScheduling
 @SpringBootApplication
 public class MaviKargaApplication {
+
+	// aws ec2 에서 한국 타임존으로 변경 위해
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MaviKargaApplication.class, args);
